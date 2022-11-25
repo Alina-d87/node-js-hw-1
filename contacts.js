dad94241602b05028b8783a26445456839f409d1
Alina Domnich <78098869+Alina-d87@users.noreply.github.com>
2022-11-25 17:27:19 +0200

    first fn

--- contacts.js
+++ contacts.js
@@ -1,1 +1,33 @@
-const fs = require("fs").promises;
+const fs = require("fs/promises");
+
+const path = require("path");
+
+const contactsPath = "./db/contacts.json";
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  async ({ contactsPath, action }) => {
+    switch (action) {
+      case read:
+        const list = await fs.readFile(contactsPath);
+        console.log(list);
+        break;
+    }
+  };
+}
+
+listContacts({ contactsPath, action: "read" });
+
+function getContactById(contactId) {
+  // ...твій код
+}
+
+function removeContact(contactId) {
+  // ...твій код
+}
+
+function addContact(name, email, phone) {
+  // ...твій код
+}
+
+module.exports = { listContacts };
